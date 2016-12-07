fc2584515e24702694502a54ec96cba71d388edc
Chad Billman <chad@pennmanor.net>
2016-12-07 16:20:25 -0500

    Remove hardcoded api keys + file paths

--- php/twit.php
+++ php/twit.php
@@ -1,13 +1,9 @@
 <?php
+require_once("config.php");
 session_start();
 require_once("twitteroauth/twitteroauth/twitteroauth.php"); //Path to twitteroauth library
 
-$twitteruser = "pennmanor";
-$notweets = 2;
-$consumerkey = "pR7MrMSBX6nAIaHwyVp1A";
-$consumersecret = "S5FOxgvWLd7ig05aHtQghtY9b7rjXZm9svEDLU3E08";
-$accesstoken = "35283009-rODeLTE6NKCNDMWHlBSwob8wBnGuIt8iGTyTv0K37";
-$accesstokensecret = "Zj6LRwLDJn7yZZ5jU28kMjxIQP6kURk2EiQJTYcA";
+$notweets = 3;
 
 function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
@@ -19,7 +15,7 @@ $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $acces
 $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
 
 echo json_encode($tweets);
-$file = "/var/www/mainsite/a/homepage/php/pennmanor-tweets.txt";
+
 $fh = fopen($file, 'w') or die("can't open file");
 fwrite($fh, json_encode($tweets));
 fclose($fh);
